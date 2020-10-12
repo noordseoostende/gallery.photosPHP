df37c6c49bf02ab57fa8ea53f5fc4143b7a1de6f
noordseoostende <58941189+noordseoostende@users.noreply.github.com>
2020-10-12 12:40:24 +0200

    Add files via upload

---
+++ cms/admin/includes/admin_content.php
@@ -0,0 +1,49 @@
+      <div class="container-fluid">
+<!-- Page Heading -->
+            <div class="row">
+               <div class="col-lg-12">
+                   <h1 class="page-header">
+                     Admin
+                    <small>Subheading</small>
+                   </h1>
+        <?php
+
+        /*$result_set = User::find_all_users();
+        while ($row = mysqli_fetch_array($result_set)){
+            echo $row['username'] ."<br>";
+
+        }*/
+
+        /*$found_user = User::find_user_by_id(3);
+
+        $user = User::instantation($found_user);
+
+
+        echo $user->username;
+
+        echo "<br>";*/
+
+        /*$users = User::find_all_users();
+
+        foreach ($users as $user) {
+
+            echo $user->id . "<br>";
+        }*/
+        $found_user = User::find_user_by_id(3);
+        echo $found_user->username;
+
+        ?>
+                    <ol class="breadcrumb">
+                      <li>
+                         <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
+                      </li>
+                      <li class="active">
+                         <i class="fa fa-file"></i> Blank Page
+                      </li>
+                    </ol>
+              </div>
+         </div>
+<!-- /.row -->
+
+     </div>
+<!-- /.container-fluid -->
